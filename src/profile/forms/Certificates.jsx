161328bed9a1fb5acd08433cb92b857bcd33f5bd
Aung Myo Kyaw <git@aungmyokyaw.anonaddy.me>
2023-08-25 16:45:51 +0630

    feat: cert url update for production

--- src/profile/forms/Certificates.jsx
+++ src/profile/forms/Certificates.jsx
@@ -70,7 +70,7 @@ class Certificates extends React.Component {
     const externalCertificateUrl = (() => {
 
       let URL_DIVIDER = "/certificates/"
-      let externalCertBaseURL = "https://sdl-cert-dev.pagewerkz.com/certificate/"
+      let externalCertBaseURL = "https://certificate.sdl.nodma.com/certificate/"
       let certId = ""
   
       if(downloadUrl != "") {
